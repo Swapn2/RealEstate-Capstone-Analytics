7242ccdefa8be90c79648780b68fcd5195cbd8b5
Swapn Gupta <120560061+Swapn2@users.noreply.github.com>
2025-06-21 02:39:27 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.set_page_config(
+     page_title = 'Hello',
+     page_icon = '$$',
+ )
+
+st.write('# welcome to stream lit :>')
+# st.sidebar.success('select a demo')
+
+
+
+
+
+##
