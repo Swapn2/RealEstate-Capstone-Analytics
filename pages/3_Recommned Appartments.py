18b2504ce67f4db0b8d7476677245a00cf1e043c
Swapn Gupta <120560061+Swapn2@users.noreply.github.com>
2025-09-28 01:24:39 +0530

    Update 3_Recommned Appartments.py

--- pages/3_Recommned Appartments.py
+++ pages/3_Recommned Appartments.py
@@ -5,11 +5,17 @@ import pickle
 
 st.set_page_config(page_title='Recommend Apartments')
 
-location_df = pickle.load(open('C:/Users/SWAPN/OneDrive/Desktop/DS_projet/RealEstate-Capstone-Analytics/location_distance.pkl' , 'rb'))
-cosine_sim1 = pickle.load(open('C:/Users/SWAPN/OneDrive/Desktop/DS_projet/RealEstate-Capstone-Analytics/cosine_sim1.pkl' , 'rb'))
-cosine_sim2 = pickle.load(open('C:/Users/SWAPN/OneDrive/Desktop/DS_projet/RealEstate-Capstone-Analytics/cosine_sim2.pkl' , 'rb'))
-cosine_sim3 = pickle.load(open('C:/Users/SWAPN/OneDrive/Desktop/DS_projet/RealEstate-Capstone-Analytics/cosine_sim3.pkl' , 'rb'))
-# import os
+# location_df = pickle.load(open('C:/Users/SWAPN/OneDrive/Desktop/DS_projet/RealEstate-Capstone-Analytics/location_distance.pkl' , 'rb'))
+# cosine_sim1 = pickle.load(open('C:/Users/SWAPN/OneDrive/Desktop/DS_projet/RealEstate-Capstone-Analytics/cosine_sim1.pkl' , 'rb'))
+# cosine_sim2 = pickle.load(open('C:/Users/SWAPN/OneDrive/Desktop/DS_projet/RealEstate-Capstone-Analytics/cosine_sim2.pkl' , 'rb'))
+# cosine_sim3 = pickle.load(open('C:/Users/SWAPN/OneDrive/Desktop/DS_projet/RealEstate-Capstone-Analytics/cosine_sim3.pkl' , 'rb'))
+# # import os
+
+location_df  = pickle.load(open('location_distance.pkl', 'rb'))
+cosine_sim1  = pickle.load(open('cosine_sim1.pkl', 'rb'))
+cosine_sim2  = pickle.load(open('cosine_sim2.pkl', 'rb'))
+cosine_sim3  = pickle.load(open('cosine_sim3.pkl', 'rb'))
+
 
 
 def recommend_properties_with_scores(property_name, top_n=247):
@@ -62,5 +68,6 @@ if st.button('Recommend'):
     recommendation_df = recommend_properties_with_scores(selected_apartment)
 
     st.dataframe(recommendation_df.head())
+
 
 
